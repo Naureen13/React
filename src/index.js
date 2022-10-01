0212604762f315e0ba1b280515c405c69d7fa977
Naureen Garg <105071935+Naureen13@users.noreply.github.com>
2022-10-01 21:20:48 +0530

    Learning react One step at a time

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
 import React from "react";
 import ReactDOM from "react-dom";
+const img = "https://picsum.photos/300";
 
 ReactDOM.render(
   <div>
-    <h1>Favourite YouTube Channels</h1>
-    <ul>
-      <li>Nas Daily</li>
-      <li>ZHC</li>
-      <li>SlayyPoint</li>
-    </ul>
-  </div>, document.getElementById("root")
-)
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite TV Series
+    </h1>
+    <img alt="random" src={img + "?grayscale"} />`
+    <img
+      className="abc"
+      src="https://ntvb.tmsimg.com/assets/p10700229_b_h10_aa.jpg?w=1280&h=720"
+      alt=""
+    />
+    <img
+      className="abc"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwCPK2dF2D830wzzuLPVnQoPx1TRTycSqUw6Nq_0r0YsxZ4utuPy2LLSe7911XVcDXeSI&usqp=CAU"
+      alt=""
+    />
+    <img
+      className="abc"
+      src="https://occ.a.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZ9awAkb-r_d-yhRCDCtVaahihZ2QxxNtiobvrxDsHE6Jq7KzsLE5eBTJSeM1hU2zxzN_iQWMYpgPnOdLV5H5_5N2ubJvVIMYlE8F-Jn22pRAj_IPv1WNEadBKFqBN9UDe4nCg.jpg?r=c68"
+      alt=""
+    />
+  </div>,
+  document.getElementById("root")
+);
